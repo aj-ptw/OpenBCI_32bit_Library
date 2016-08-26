0fa03ff9bc991eb062258a79db9208954ebe3fa2
AJ Keller <pushtheworldllc@gmail.com>
2016-08-26 16:20:19 -0400

    Add wifi support

--- examples/BoardWithWifi/BoardWithWifi.ino
+++ examples/BoardWithWifi/BoardWithWifi.ino
@@ -8,49 +8,30 @@ void setup() {
   // Bring up the OpenBCI Board
   board.begin();
   board.beginSecondarySerial();
-
+  board.curBoardMode = OPENBCI_BOARD_MODE_WIFI;
 }
 
 void loop() {
 
-    if (board.streaming) {
-        if (board.channelDataAvailable) {
-            // Read from the ADS(s), store data, set channelDataAvailable flag to false
-            board.updateChannelData();
+  if (board.streaming) {
+    if (board.channelDataAvailable) {
+      // Read from the ADS(s), store data, set channelDataAvailable flag to false
+      board.updateChannelData();
 
-            if (board.timeSynced) {
-                board.sendChannelDataWithTimeAndRawAux();
-            } else {
-                // Send standard packet with channel data
-                board.sendChannelDataWithRawAux();
-            }
-        }
+      board.sendChannelDataWithRawAux();
+
     }
+  }
 
-    // Check the serial port for new data
-    if (Serial0.available()) {
-        char newChar = board.getCharSerial0();
-        // Read one char and process it
-        board.processChar(newChar);
+  // Check the serial port for new data
+  if (board.hasDataSerial0()) {
+    // Read one char and process it
+    board.processChar(board.getCharSerial0());
+  }
+  if (board.hasDataSerial1()) {
+    // Read one char and process it
+    board.getCharSerial1();
+    // board.processChar();
+  }
 
-        if (newChar == 'h') {
-            Serial1.print("AT");
-        } else if (newChar == 'k') {
-            Serial1.print("AT+RST");
-        } else if (newChar == 'n') {
-            Serial1.print("AT+GMR");
-        } else if (newChar == 'N') {
-            Serial1.print("AT+CWMODE=3");
-        } else if (newChar == 'o') {
-            Serial1.print("AT+CWJAP="",""");
-        } else if (newChar == 'O') {
-            Serial1.print("AT+CWJAP?");
-        } else if (newChar == 'V') {
-            Serial1.print("AT+CWLAP");
-        }
-    }
-
-    if (board.hasDataSerial1()) {
-        Serial0.write(Serial1.read());
-    }
 }
